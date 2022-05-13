bdc97f60651c329f2557b917ab8661e4d5dc9e77
Anton Lukin <anton@lukin.me>
2022-05-13 12:07:02 -0300

    add new custom styles

---
+++ public/single/sergify-it/functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * sergify-it: custom functions
+ */
+
+if (!defined('WPINC')) {
+    die;
+}
+
+/**
+ * Add custom styles
+ */
+add_action('wp_enqueue_scripts', function() {
+    $version = '1.0';
+
+    // Get styles path
+    $styles = plugin_dir_url(__FILE__) . 'styles.min.css';
+
+    if(defined('WP_DEBUG') && true === WP_DEBUG) {
+        $version = date('U');
+    }
+
+    // Let's add the file
+    wp_enqueue_style('knife-custom-' . basename(__DIR__), $styles, ['knife-theme'], $version);
+});
---
+++ public/special/heineken/functions.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * heineken: special functions
+ */
+
+if (!defined('WPINC')) {
+    die;
+}
+
+
+/**
+ * Add styles
+ */
+add_action('wp_enqueue_scripts', function() {
+    $version = '1.0';
+
+    // Get styles path
+    $styles = plugin_dir_url(__FILE__) . 'styles.min.css';
+
+    if(defined('WP_DEBUG') && true === WP_DEBUG) {
+        $version = date('U');
+    }
+
+    // Let's add the file if exists
+    wp_enqueue_style('knife-special-' . basename(__DIR__), $styles, ['knife-theme'], $version);
+});
+
+
+/**
+ * Set template for archive posts
+ */
+add_action('archive_template', function($template) {
+    return plugin_dir_path(__FILE__) . 'archive.php';
+});
+
+
+/**
+ * Add button to post content
+ */
+add_action('the_content', function($content) {
+    global $post;
+
+    $slug = basename(__DIR__);
+
+    if(has_term($slug, 'special', $post) && is_main_query() && is_singular('post')) {
+        $promo_link = sprintf(
+            '<figure class="figure figure--promo"><a class="button" href="%s">%s <strong>%s</strong></a>',
+            esc_url(get_term_link($slug, 'special')),
+            _x('Специальный проект', 'special: heineken', 'knife-customs'),
+            _x('компании Heineken и журнала «НОЖ»', 'special: heineken', 'knife-customs')
+        );
+
+        $content = $content . $promo_link;
+    }
+
+    return $content;
+});
