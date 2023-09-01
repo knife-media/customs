906bbd6359da688e5fc124fec5d36f9a00acf45b
Anton Lukin <anton@lukin.me>
2023-09-01 20:17:35 +0300

    add alchemy-cards custom styles

---
+++ public/single/alchemy-cards/functions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * alchemy-cards: custom functions
+ */
+
+if (!defined('WPINC')) {
+    die;
+}
+
+/**
+ * Add custom styles
+ */
+add_action('wp_enqueue_scripts', function() {
+    $version = '1.0';
+
+    // Get page slug
+    $slug = basename(__DIR__);
+
+    // Get styles path
+    $styles = content_url("customs/single/{$slug}/styles.min.css");
+
+    if(defined('WP_DEBUG') && true === WP_DEBUG) {
+        $version = date('U');
+    }
+
+    // Let's add the file
+    wp_enqueue_style('knife-custom-' . $slug, $styles, ['knife-theme'], $version);
+});
+
